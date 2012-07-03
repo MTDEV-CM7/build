5faf597a8ee257c9c300d9e712cf9e30dd73d990
dsmryder <dsmryder@yahoo.com>
2012-07-03 09:02:50 -0300

    My audio pack

--- target/product/small_base.mk
+++ target/product/small_base.mk
@@ -30,7 +30,7 @@ PRODUCT_LOCALES := en_US
 
 # Pick up some sounds - stick with the short list to save space
 # on smaller devices.
-$(call inherit-product-if-exists, frameworks/base/data/sounds/OriginalAudio.mk)
+$(call inherit-product-if-exists, frameworks/base/data/sounds/dsmryderAudio.mk)
 
 # Get the list of languages.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_small.mk)
